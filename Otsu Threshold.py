e126dbc5b10494a4dd20bc03638fd385a304a57d
ukt <ufukkt2699@hotmail.com>
2019-12-27 19:56:32 +0300

    Otsu Threshold

--- Otsu Threshold.py
+++ Otsu Threshold.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('iha.mp4')
+cap = cv2.VideoCapture(0)
 
 while (1):
     ret, img = cap.read()
@@ -10,12 +10,12 @@ while (1):
     processedimg = cv2.convertScaleAbs(img)
     ret1, th1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
     ret2, th2 = cv2.threshold(gray, 0, 255, (cv2.THRESH_BINARY + cv2.THRESH_OTSU))
-    #blur = cv2.GaussianBlur(processedimg, (5, 5), 0)
-    ret3, th3 = cv2.threshold(gray, 0, 255, (cv2.THRESH_BINARY + cv2.THRESH_OTSU))
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    ret3, th3 = cv2.threshold(blur, 0, 255, (cv2.THRESH_BINARY + cv2.THRESH_OTSU))
 
-    cv2.imshow('deneHme',th1)
-    cv2.imshow('deneKme', th2)
-    cv2.imshow('deneme', th3)
+    cv2.imshow('Global Tresholding (v=127)',th1)
+    cv2.imshow('Otsus Tresholding1', th2)
+    cv2.imshow('Otsus Tresholding2', th3)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
