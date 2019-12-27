d8dc0051060bb19f2d59e5dbc890f89b781f8c35
ukt <ufukkt2699@hotmail.com>
2019-12-27 20:03:43 +0300

    Hareketsiz arkaplan sildirme

--- Hareketsiz Arkaplan Silme.py
+++ Hareketsiz Arkaplan Silme.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('iha.mp4')
+cap = cv2.VideoCapture(0)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 fgbg = cv2.createBackgroundSubtractorMOG2()
+
 while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow('GORUNTU',fgmask)
+    cv2.imshow('görüntü',fgmask)
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
@@ -15,6 +16,6 @@ while(1):
     if cv2.waitKey(27) == ord('q'):
         break
 
-cv2.imwrite('frame.png',fgbg)
+
 cap.release()
 cv2.destroyAllWindows()
